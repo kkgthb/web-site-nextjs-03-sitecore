79ed253fa3907ab361de973988f205033809cdf6
Katie Kodes <44843658+kkgthb@users.noreply.github.com>
2024-10-15 07:16:23 -0500

    Managed to wrap my fragment in 1 Sitecore-specific tag without breaking anything, but it doesn't yet do anything useful.

--- src/web/src/pages/[[...path]].page.tsx
+++ src/web/src/pages/[[...path]].page.tsx
@@ -1,10 +1,23 @@
 import React from "react"
+import { SitecorePageProps } from "../lib/sc-page-props";
+import {
+    ComponentPropsContext,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
-const MyPage = (): React.JSX.Element => {
-    return <React.Fragment>
-        <h1>Header goes here</h1>
-        <p>Hello, world!</p>
-    </React.Fragment>
+const MyPage = ({
+    notFound,
+    componentProps,
+    layoutData,
+    headLinks,
+}: SitecorePageProps): React.JSX.Element => {
+    return (
+        <ComponentPropsContext value={componentProps}>
+            <React.Fragment>
+                <h1>Header goes here</h1>
+                <p>Hello, world!</p>
+            </React.Fragment>
+        </ComponentPropsContext>
+    );
 }
 
 export default MyPage;
