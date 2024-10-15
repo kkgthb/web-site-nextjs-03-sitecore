c0f3f07f26348331b619f264c18c2ab0dde9ec5d
Katie Kodes <44843658+kkgthb@users.noreply.github.com>
2024-10-15 07:22:15 -0500

    Scootched in one more dummy SC tag I don't understand without breaking H1 output.

--- src/web/src/pages/[[...path]].page.tsx
+++ src/web/src/pages/[[...path]].page.tsx
@@ -2,20 +2,20 @@ import React from "react"
 import { SitecorePageProps } from "../lib/sc-page-props";
 import {
     ComponentPropsContext,
+    SitecoreContext,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 const MyPage = ({
-    notFound,
     componentProps,
-    layoutData,
-    headLinks,
 }: SitecorePageProps): React.JSX.Element => {
     return (
         <ComponentPropsContext value={componentProps}>
-            <React.Fragment>
-                <h1>Header goes here</h1>
-                <p>Hello, world!</p>
-            </React.Fragment>
+            <SitecoreContext componentFactory={null}>
+                <React.Fragment>
+                    <h1>Header goes here</h1>
+                    <p>Hello, world!</p>
+                </React.Fragment>
+            </SitecoreContext>
         </ComponentPropsContext>
     );
 }
