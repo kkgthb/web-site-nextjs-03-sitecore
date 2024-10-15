d3401b414b4e12595b490d45cf37e65e6463fc17
Katie Kodes <44843658+kkgthb@users.noreply.github.com>
2024-10-15 08:01:46 -0500

    Took an unused import out of datestamp unit test

--- src/web/src/lib/get-hydration-friendly-datestamp.test.ts
+++ src/web/src/lib/get-hydration-friendly-datestamp.test.ts
@@ -1,12 +1,12 @@
 // Credit:  https://kentcdodds.com/blog/how-to-test-custom-react-hooks via https://www.reddit.com/r/react/comments/1d3ukbc/comment/l6a9g4a/
 
 import { describe, it, expect } from 'vitest';
-import { renderHook, act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import useFormattedDate from './get-hydration-friendly-datestamp';
 
 describe('Date stamp utility', () => {
-    const rawNow = Date.now()
-    const stringifiedNow = new Date(rawNow).toLocaleString("en-US");
+    const rawNow: number = Date.now()
+    const stringifiedNow: string = new Date(rawNow).toLocaleString("en-US");
 
     it('should be a string showing the current date and time', () => {
         const { result } = renderHook(() => useFormattedDate(rawNow));
